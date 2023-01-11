ffc97c65e98160d2723462e7150b1b0820784927
franiglesias571 <franiglesias571@gmail.com>
2023-01-11 18:39:07 -0300

    agrego_itemcount

--- src/components/itemlistcontainer/itemlistcontainer.jsx
+++ src/components/itemlistcontainer/itemlistcontainer.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Title from '../title/title';
+import ItemCount from '../itemcount/itemcount';
 
-export const ItemListContainer = (props) => {
+export const ItemListContainer = ( {text}  ) => {
+    
+    const onAdd= (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return (
-        <Title greeting= {props.text}  /> // IMPORT DEL title.JSX DE CARPETA TITLE 
-    )
+        <>
+        <Title greeting= {text} /> {/*IMPORT DEL title.JSX DE CARPETA TITLE*/}
+        <ItemCount initial={3} stock={5} onAdd={onAdd} />                   {/* indicador del carrito de compras*/}
+        </> 
+    );
 }
